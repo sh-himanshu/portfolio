1b810d2a029d2303759ef952c94479ffa7db6a8e
Himanshu Sharma <himanshu@raftlabs.co>
2022-07-10 20:56:24 +0530

    :recycle: refactor(frontend): remove unused dependencies

--- apps/frontend/components/index.tsx
+++ apps/frontend/components/index.tsx
@@ -4,4 +4,3 @@ export { default as Hero } from './Hero/Hero';
 export { default as Layout } from './Layout/Layout';
 export { default as MyTimeline } from './MyTimeline/MyTimeline';
 export { default as Project } from './Project/Project';
-export { default as Slider } from './Slider/Slider';
